76c2a2fdffd5f142655dff3f3c9ba21a365b5f10
arielyip <arielyip@amazon.my>
2025-01-14 12:53:19 +0800

    Updated formatting and content support

--- functions/main.py
+++ functions/main.py
@@ -21,6 +21,8 @@ bucket = s3.Bucket(BUCKET_NAME)
 # Http 
 http = urllib3.PoolManager()
 
+system_prompt = "You are a helpful AI Assistant that will answer all questions and queries as accurately as possible. You can use only the following HTML tags to highligh the response text: <b>, <i>, <u>, <code>, <pre>, <a>; otherwise the answer must be a plain text without formatting."
+
 def lambda_handler(event,context):
     ''' Main Logic '''
 
@@ -50,69 +52,77 @@ def lambda_handler(event,context):
     message_history.append(message)
 
     # Send to Bedrock
-    response = bedrock.converse_stream(
-        modelId=model["modelId"],
-        messages=message_history,
-        inferenceConfig=config
-    )
-
-    stream_response = response.get('stream')
-
-    '''Concatenate every 10 events into 1 chunk. 1st chunk performs sendMessage to telegram, subsequent chunk edits the message'''
-    current_text_chunk = []
-    accumulated_text = []
-    text_counter = 0
-    message_id = None
-
-    for event in stream_response:
-        if 'contentBlockDelta' in event:
-            text = event['contentBlockDelta']['delta']['text']
-            current_text_chunk.append(text)
-            text_counter += 1
-
-            if text_counter % 20 == 0:
-                concatenated_text = ''.join(current_text_chunk)
-                accumulated_text.append(concatenated_text)
-                full_text = ''.join(accumulated_text)
-                print(full_text)
-
-                if message_id is None:
-                    response = telegram_action(BOT_TOKEN, 'sendMessage', {'chat_id': chat_id, 'text': full_text})
-                    message_id = response['result']['message_id']
-                else:
-                    telegram_action(BOT_TOKEN, 'editMessageText', {'chat_id': chat_id, 'message_id': message_id, 'text': full_text})
-                
-                current_text_chunk = []
-    
-    # Concatenate remainder events and send or edit message to Telegram
-    if current_text_chunk:
-        final_chunk = ''.join(current_text_chunk)
-        accumulated_text.append(final_chunk)
-        full_text = ''.join(accumulated_text)
-        print(full_text)
-
-        if message_id is None:
-            response = telegram_action(BOT_TOKEN, 'sendMessage', {'chat_id': chat_id, 'text': full_text})
-            message_id = response['result']['message_id']
-        else:
-            telegram_action(BOT_TOKEN, 'editMessageText', {'chat_id': chat_id, 'message_id': message_id, 'text': full_text})
-
-    message_history.append(
-        {
-            "role": "assistant", 
-            "content": [{
-                "text":full_text
-                }
-            ]
-        }
-    )
-
-    # Saves the message history to S3
     try:
-        bucket.put_object(Key=f"{chat_id}-history.json", Body=json.dumps(message_history))
+        response = bedrock.converse_stream(
+            modelId=model["modelId"],
+            messages=message_history,
+            inferenceConfig=config,
+            system=[{"text":system_prompt}],
+        )
+
+        stream_response = response.get('stream')
+
+        '''Concatenate every 10 events into 1 chunk. 1st chunk performs sendMessage to telegram, subsequent chunk edits the message'''
+        current_text_chunk = []
+        accumulated_text = []
+        text_counter = 0
+        message_id = None
+
+        for event in stream_response:
+            if 'contentBlockDelta' in event:
+                text = event['contentBlockDelta']['delta']['text']
+                current_text_chunk.append(text)
+                text_counter += 1
+
+                if text_counter % 20 == 0:
+                    concatenated_text = ''.join(current_text_chunk)
+                    accumulated_text.append(concatenated_text)
+                    full_text = ''.join(accumulated_text)
+                    print(full_text)
+
+                    if message_id is None:
+                        response = telegram_action(BOT_TOKEN, 'sendMessage', {'chat_id': chat_id, 'text': full_text, 'parse_mode': 'HTML'})
+                        message_id = response['result']['message_id']
+                    else:
+                        telegram_action(BOT_TOKEN, 'editMessageText', {'chat_id': chat_id, 'message_id': message_id, 'text': full_text, 'parse_mode': 'HTML'})
+                    
+                    current_text_chunk = []
+        
+        # Concatenate remainder events and send or edit message to Telegram
+        if current_text_chunk:
+            final_chunk = ''.join(current_text_chunk)
+            accumulated_text.append(final_chunk)
+            full_text = ''.join(accumulated_text)
+            print(full_text)
+
+            if message_id is None:
+                response = telegram_action(BOT_TOKEN, 'sendMessage', {'chat_id': chat_id, 'text': full_text, 'parse_mode': 'HTML'})
+                message_id = response['result']['message_id']
+            else:
+                telegram_action(BOT_TOKEN, 'editMessageText', {'chat_id': chat_id, 'message_id': message_id, 'text': full_text, 'parse_mode': 'HTML'})
+
+        message_history.append(
+            {
+                "role": "assistant", 
+                "content": [{
+                    "text":full_text
+                    }
+                ]
+            }
+        )
+
+        # Saves the message history to S3
+        try:
+            bucket.put_object(Key=f"{chat_id}-history.json", Body=json.dumps(message_history))
+        except:
+            logger.info("Unable to save message history")
+            logger.info(f"Message Hisotry: {message_history}")
     except:
-        logger.info("Unable to save message history")
-        logger.info(f"Message Hisotry: {message_history}")
+        logger.info("Unable to send message to Bedrock")
+        logger.info(f"Message: {message}")
+        # log the error message
+        logger.info(f"Error: {response}")
+        telegram_action(BOT_TOKEN, 'sendMessage', {'chat_id': chat_id, 'text': "Sorry, I'm unable to process this message at this time"})
 
     return {
         'statusCode': 200,
